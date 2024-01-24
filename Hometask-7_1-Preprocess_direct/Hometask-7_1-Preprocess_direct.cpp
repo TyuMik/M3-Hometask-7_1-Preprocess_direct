a9b7c7f7e26298d66525bb3fb29b9c15b6e0fabe
Mikhail <miktyu88@gmail.com>
2024-01-24 22:27:55 +0300

    Initial commit

---
+++ Hometask-7_1-Preprocess_direct/Hometask-7_1-Preprocess_direct.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#define MODE 2
+
+#if MODE == 1
+int add(int a, int b) {
+	return (a + b);
+}
+#endif
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	#ifdef MODE 
+		#if MODE == 0
+			std::cout << "Работаю в режиме тренировки" << std::endl;
+		#elif MODE == 1 
+			std::cout << "Работаю в боевом режиме" << std::endl;
+			int x, y;
+			std::cout << "Введите число 1: ";
+			std::cin >> x;
+			std::cout << "Введите число 2: ";
+			std::cin >> y;
+			std::cout << "Результат сложения: " << add(x, y) << std::endl;
+		#else 
+			std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+			return 0;
+		#endif
+	#else 
+		#error "Определите MODE!"
+	#endif
+
+	return 0;
+}
+
